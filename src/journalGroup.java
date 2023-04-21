2add91e9c2a788ccdb999c86e27339a17ee8f3ec
DenysRybak <mribak1983@gmail.com>
2023-04-21 22:25:17 +0300

    lab5add

---
+++ src/journalGroup.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class journalGroup {
+    public static void main(String[] args) {
+        List<Student>studentList = new LinkedList<>();
+
+
+        Student student = new Student("Kosiba", "Ayaka",45);
+        Student student1 = new  Student ("Vorsh", "Oleh", 50);
+        Student student2 = new Student("Beshe", "Martin",71);
+
+       student.Check();
+       student1.Check();
+       student2.Check();
+    }
+}
